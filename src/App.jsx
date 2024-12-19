a47964f3098df3650452f63ce78a115550bed1af
MosfiqurRubel <rubel.airin@gmail.com>
2024-12-20 01:14:01 +0600

    tailwind react project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="p-4 dark:bg-slate-700">
+        <h1 className="text-3xl font-bold underline text-green-600">
+          Hello world!
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
