ac6ef1f27e3f733b9906ae97ac61e596c60ad809
MosfiqurRubel <rubel.airin@gmail.com>
2025-01-01 23:12:53 +0600

    Update structures

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <div className="p-4 dark:bg-slate-700">
-        <h1 className="text-3xl font-bold underline text-green-600">
+        <h1 className="text-3xl font-bold underline text-green-600 hover:text-purple-500 text-primary">
           Hello world!
         </h1>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        plex: ['"IBM Plex Sans", serif'],
+      },
+      colors: {
+        primary: "rgba(0, 0, 0, 0.85)",
+      },
+    },
   },
   plugins: [],
 };
